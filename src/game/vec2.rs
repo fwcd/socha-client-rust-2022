8452af0a9a0764d1e0964164a5a2ab9f71e1afed
fwcd <fwcdmail@gmail.com>
2022-01-30 15:57:39 +0100

    Overload arithmetic operators for Vec2

--- src/game/vec2.rs
+++ src/game/vec2.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{fmt, ops::{Add, Sub, Mul, Div, DivAssign, MulAssign}};
 
 use crate::util::{Element, SCError, SCResult};
 
@@ -11,6 +11,11 @@ pub struct Vec2 {
 
 impl Vec2 {
     #[inline]
+    pub fn zero() -> Self {
+        Self::new(0, 0)
+    }
+
+    #[inline]
     pub fn new(x: usize, y: usize) -> Self {
         Self { x, y }
     }
@@ -20,6 +25,60 @@ impl Vec2 {
 
     #[inline]
     pub fn y(self) -> usize { self.y }
+}
+
+impl Add for Vec2 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl Sub for Vec2 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self::new(self.x - rhs.x, self.y - rhs.y)
+    }
+}
+
+impl Mul<usize> for Vec2 {
+    type Output = Self;
+
+    fn mul(self, rhs: usize) -> Self {
+        Self::new(self.x * rhs, self.y * rhs)
+    }
+}
+
+impl Mul<Vec2> for usize {
+    type Output = Vec2;
+
+    fn mul(self, rhs: Vec2) -> Vec2 {
+        Vec2::new(self * rhs.x, self * rhs.y)
+    }
+}
+
+impl MulAssign<usize> for Vec2 {
+    fn mul_assign(&mut self, rhs: usize) {
+        self.x *= rhs;
+        self.y *= rhs;
+    }
+}
+
+impl Div<usize> for Vec2 {
+    type Output = Self;
+
+    fn div(self, rhs: usize) -> Self {
+        Self::new(self.x / rhs, self.y / rhs)
+    }
+}
+
+impl DivAssign<usize> for Vec2 {
+    fn div_assign(&mut self, rhs: usize) {
+        self.x /= rhs;
+        self.y /= rhs;
+    }
 }
 
 impl fmt::Display for Vec2 {
