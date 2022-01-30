c6635d177b327a5b1eea077467b03780f795fb87
fwcd <fwcdmail@gmail.com>
2022-01-30 03:15:43 +0100

    Add string conversions for piece type

--- src/game/piece_type.rs
+++ src/game/piece_type.rs
@@ -1,3 +1,8 @@
+use std::str::FromStr;
+use std::fmt;
+
+use crate::util::{SCError, SCResult};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum PieceType {
     /// Moves only diagonally forwards.
@@ -14,5 +19,30 @@ impl PieceType {
     /// Checks whether a piece is lightweight. Only the 'robbe' is non-light.
     pub fn is_light(&self) -> bool {
         !matches!(self, Self::Robbe)
+    }
+}
+
+impl fmt::Display for PieceType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PieceType::Herzmuschel => write!(f, "Herzmuschel"),
+            PieceType::Moewe => write!(f, "Moewe"),
+            PieceType::Seestern => write!(f, "Seestern"),
+            PieceType::Robbe => write!(f, "Robbe"),
+        }
+    }
+}
+
+impl FromStr for PieceType {
+    type Err = SCError;
+
+    fn from_str(s: &str) -> SCResult<Self> {
+        match s {
+            "Herzmuschel" => Ok(Self::Herzmuschel),
+            "Moewe" => Ok(Self::Moewe),
+            "Seestern" => Ok(Self::Seestern),
+            "Robbe" => Ok(Self::Robbe),
+            _ => Err(SCError::UnknownVariant(format!("Unknown piece type {}", s))),
+        }
     }
 }
