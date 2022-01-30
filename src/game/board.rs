90cdb21dd2c9318548c56d6ff9f017efc6b90436
fwcd <fwcdmail@gmail.com>
2022-01-30 03:28:13 +0100

    Parse board

--- src/game/board.rs
+++ src/game/board.rs
@@ -1,16 +1,35 @@
+use std::collections::HashMap;
+
 use crate::util::{Element, SCError, SCResult};
 
-/// A game board storing the pieces.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+use super::{Coords, Piece};
+
+/// An 8x8 game board storing the pieces (8 pieces per team).
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Board {
-    // TODO
+    /// The pieces on the board keyed by position.
+    pieces: HashMap<Coords, Piece>,
+}
+
+impl Board {
+    /// The pieces on the board.
+    pub fn pieces(&self) -> &HashMap<Coords, Piece> { &self.pieces }
 }
 
 impl TryFrom<&Element> for Board {
     type Error = SCError;
 
     fn try_from(elem: &Element) -> SCResult<Self> {
-        // TODO
-        Ok(Board {})
+        Ok(Board {
+            pieces: elem
+                .child_by_name("pieces")?
+                .childs_by_name("entry")
+                .map(|e| {
+                    let coords = Coords::try_from(e.child_by_name("coordinates")?)?;
+                    let piece = Piece::try_from(e.child_by_name("piece")?)?;
+                    Ok((coords, piece))
+                })
+                .collect::<SCResult<_>>()?,
+        })
     }
 }
